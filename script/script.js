1cad83cbc9a7f90567f2d529f5247e218e783c8a
Rodrigo Rodrigues <rfrodrigues@inf.ufpel.edu.br>
2021-10-05 21:24:38 -0300

    fix_organizacao de html: icons script inside head

--- script/script.js
+++ script/script.js
@@ -5,4 +5,16 @@ import view from './modules/view.js'
 view()  // Ativando a View;
 
 */
+
 feather.replace() // Para ativar as imagens dentro dos botões
+
+/*
+1.INCLUIR NO BODY DO HTML PARA FUNCIONAR A FIGURA DOS BOTÕES:
+
+    <script src="https://unpkg.com/feather-icons"></script>
+
+2. ADICIONAR NO ARQUIVO SCRIPT.JS A LINHA SEGUINTE:
+
+    feather.replace()
+    
+*/
