a31545e52f6f55853726e9de36c4d213bc6ca950
Alberto Helbig <albertohelbig@gmail.com>
2021-10-11 23:01:49 -0300

    feat: add border-radius to certificates list

--- script/modules/certificates/showCertificates.js
+++ script/modules/certificates/showCertificates.js
@@ -8,9 +8,12 @@ export default function () {
         if(liAdded.classList.contains('hide')){
             liAdded.classList.remove('hide')
             liAdded.classList.add('show');
+            dropdown.classList.add('certificates-border');
         } else {
             liAdded.classList.remove('show')
             liAdded.classList.add('hide');
+            dropdown.classList.remove('certificates-border');
+
         }
     })
 
