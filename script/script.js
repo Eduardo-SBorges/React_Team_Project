5590b102d2238d43e0de2c8a0fd37e41f4eb4ce2
Alberto Helbig <albertohelbig@gmail.com>
2021-10-11 22:58:32 -0300

    feat: import certificates functions

--- script/script.js
+++ script/script.js
@@ -1,9 +1,10 @@
-/*
-
-import view from './modules/view.js'
-
-view()  // Ativando a View;
-
-*/
+import favoriteHeart from './modules/certificates/favoriteHeart.js';
+import preventButtons from './modules/preventButtons.js';
+import addCertificates from './modules/certificates/addCertificates.js';
+import showCertificates from './modules/certificates/showCertificates.js';
 
 feather.replace() // Para ativar as imagens dentro dos botões
+favoriteHeart();
+preventButtons();
+addCertificates();
+showCertificates();
